55f734e4d920d8a57a9f3fef9955db1a9af0965b
Ben W <mrwilliams20602@hotmail.com>
2019-04-12 16:09:37 -0400

    Removed connection for a messenger that was removed

--- Carepoint/Scripts/MyScripts/Site.js
+++ Carepoint/Scripts/MyScripts/Site.js
@@ -24,40 +24,4 @@
             ]
         }
     });
-
-    var messanger = $.connection.messangerHub;
-    console.log(messanger);
-    //call FROM server
-    messanger.client.broadcastmessage = function (name, message) {
-        var encodedName = $('<div>').text(name).html();
-        var encodedMsg = $('<div>').text(message);
-        //$("#messages").append(encodedName);
-        $("#incoming-messages").append(encodedMsg);
-    };
-    $.connection.hub.start().done(function () {
-        $("#btn-sendChat").click(function (sender) {
-            var senderMesaage = $("#userMessage").val();
-            //call to server
-            messanger.server.send("Ben", senderMesaage);
-            $("#userMessage").val(" ").focus();
-        });
-    }).fail(function () {
-        alert("Needs work...");
-        });
-
-    $("#userMessage").keydown(function (key) {
-        if (key.keyCode === 13) {
-            $("#btn-sendChat").click();
-        }
-    });
-
 });
-$("#openBtn").click(function (args) {
-    //$(".grower").toggleClass("grow-large");
-    var dadiv = $("#dadiv");
-    dadiv.addClass("grower");
-    $("#grower").append(dadiv);
-    dadiv.show();
-    dadiv.toggleClass("grow-large");
-});
-
